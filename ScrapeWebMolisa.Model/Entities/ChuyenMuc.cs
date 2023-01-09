cb684612b92a2f74726a5ffabab66608b5b5cd30
loli-kawaii <hieubeu143@gmail.com>
2023-01-09 11:15:31 +0700

    update

---
+++ ScrapeWebMolisa.Model/Entities/ChuyenMuc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeWebMolisa.Model.Entities
+{
+    public class ChuyenMuc
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ChuyenMucMe { get; set; }
+        public string Mota { get; set; }
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "a455a1s4hi";
+            Console.WriteLine(test+"hina");
+
+        }
+    }
+}
